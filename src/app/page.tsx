a7a0e68809d922cd9577da567eaaad0cac9458ec
Ahmed Elbhery <ahmed@gmail.com>
2026-03-31 01:48:42 +0200

    change desctaption

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default async function HomePage(props: {
       {/* ================= HERO ================= */}
       <section className="bg-gradient-to-bl from-green-600 via-green-500 to-emerald-500 text-white py-20">
         <div className="container mx-auto px-4 text-center max-w-3xl">
-          <h1 className="text-4xl font-bold mb-4">ابحث بذكاء عن عقارك في حدائق أكتوبر</h1>
+          <h1 className="text-4xl font-bold mb-4">ابحث بذكاء عن عقارك  </h1>
           <p className="text-green-50 opacity-90">دليلك العقاري الشامل لأفضل المجمعات السكنية</p>
         </div>
       </section>
